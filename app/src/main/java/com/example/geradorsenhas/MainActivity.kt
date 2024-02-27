eb0d91da21425c6e5ab7a8f7949147733e6ef719
Nathan Lucas Santos Nicolau <98483160+Nathan-Nicolau@users.noreply.github.com>
2024-02-27 07:11:41 -0300

    Commit inicial

---
+++ app/src/main/java/com/example/geradorsenhas/MainActivity.kt
@@ -0,0 +1,48 @@
+
+package com.example.geradorsenhas
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.geradorsenhas.ui.theme.GeradorSenhasTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            App()
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun App(){
+    Scaffold(
+        containerColor = Color.Gray,
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AppPreview() {
+    GeradorSenhasTheme {
+        App()
+    }
+}
