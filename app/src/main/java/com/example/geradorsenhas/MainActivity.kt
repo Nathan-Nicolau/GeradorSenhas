9ff562b156d23832521d50fa1a38ee4f153dc03b
nathan.nicolau <nathan.nicolau@fiorilli.com.br>
2024-03-06 17:21:15 -0300

    Adições iniciais de compose

--- app/src/main/java/com/example/geradorsenhas/MainActivity.kt
+++ app/src/main/java/com/example/geradorsenhas/MainActivity.kt
@@ -5,18 +5,34 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxColors
+import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ModifierInfo
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.geradorsenhas.ui.theme.GeradorSenhasTheme
+import com.example.geradorsenhas.ui.theme.fontSora
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,11 +48,53 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun App(){
     Scaffold(
-        containerColor = Color.Gray,
-        modifier = Modifier.fillMaxSize()
+        containerColor = Color.White,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
     ) {
+        Box(modifier = Modifier
+            .fillMaxWidth()
+        ){
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()){
+                    Column(modifier = Modifier.fillMaxWidth()
+                        .border(1.5.dp, Color.LightGray, RoundedCornerShape(15))
+                        .padding(8.dp)) {
+                        Text(text = "Gerador de Senhas", fontFamily = fontSora, fontWeight = FontWeight.ExtraBold,
+                            modifier = Modifier
+                                .border(1.5.dp, Color.DarkGray, RoundedCornerShape(15))
+                                .padding(8.dp)
+                        )
+                        Row (modifier = Modifier.fillMaxWidth().padding(top = 16.dp)){
+                            Text(text = "Caracteres Especiais ?",
+                                modifier = Modifier.padding(end = 10.dp),
+                                fontFamily = fontSora, fontWeight = FontWeight.Normal)
+                            Checkbox(checked = true, onCheckedChange = validarEscolhaQtdCaracters(), modifier = Modifier
+                                .background(Color.White, RoundedCornerShape(15))
+                                .border(1.5.dp, Color.LightGray, RoundedCornerShape(15))
+                            )
+                        }
+                         Row(modifier = Modifier.fillMaxWidth().padding(top = 16.dp)){
+                             Text(text = "Com números ?",
+                                 modifier = Modifier.padding(end = 10.dp),
+                                 fontFamily = fontSora, fontWeight = FontWeight.Normal)
+                             Checkbox(checked = true, onCheckedChange = validarEscolhaQtdCaracters(), modifier = Modifier
+                                 .background(Color.White, RoundedCornerShape(15))
+                                 .border(1.5.dp, Color.LightGray, RoundedCornerShape(15))
+                             )
+                         }
 
+                    }
+            }
+        }
     }
+}
+
+fun validarEscolhaQtdCaracters(): ((Boolean) -> Unit)? {
+    return null
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/example/geradorsenhas/ui/theme/Type.kt
+++ app/src/main/java/com/example/geradorsenhas/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package com.example.geradorsenhas.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.geradorsenhas.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -31,4 +33,10 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
+)
+
+val fontSora = FontFamily(
+    Font(R.font.sora, FontWeight.Normal),
+    Font(R.font.sora_semibold, FontWeight.SemiBold),
+    Font(R.font.sora_extrabold, FontWeight.ExtraBold)
 )
