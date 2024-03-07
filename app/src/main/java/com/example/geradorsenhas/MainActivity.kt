e23304e44f871451cf3faa6812e5f436aad9d724
Nathan Lucas Santos Nicolau <98483160+Nathan-Nicolau@users.noreply.github.com>
2024-03-06 22:11:31 -0300

    Modificações iniciais de layout

--- app/src/main/java/com/example/geradorsenhas/MainActivity.kt
+++ app/src/main/java/com/example/geradorsenhas/MainActivity.kt
@@ -5,28 +5,40 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.FloatTweenSpec
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CheckboxColors
 import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageShader
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ModifierInfo
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -55,50 +67,91 @@ fun App(){
             .padding(16.dp)
     ) {
         Box(modifier = Modifier
-            .fillMaxWidth()
+            .fillMaxWidth(),
         ){
             Text(text = "Gerador de Senhas", fontFamily = fontSora, fontWeight = FontWeight.ExtraBold,
                 color = Color.White,
-                modifier = Modifier.background(Color.Blue, RoundedCornerShape(16))
+                modifier = Modifier
+                    .background(Color.Blue, RoundedCornerShape(16))
                     .padding(8.dp)
             )
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .wrapContentHeight()
-                    .padding(top = 50.dp)){
-                    Column(modifier = Modifier.fillMaxWidth()
-                        .border(1.5.dp, Color.LightGray, RoundedCornerShape(15))
+            Box(modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .padding(top = 50.dp)){
+                    Column(modifier = Modifier
+                        .fillMaxWidth()
                         .padding(8.dp)) {
-                        Row (modifier = Modifier.fillMaxWidth()){
-                            Text(text = "Caracteres Especiais ?",
-                                modifier = Modifier.padding(end = 10.dp),
-                                fontFamily = fontSora, fontWeight = FontWeight.SemiBold,
-                                color = Color.Black)
-                            Checkbox(checked = true, onCheckedChange = validarEscolhaQtdCaracters(), modifier = Modifier
-                                .background(Color.White, RoundedCornerShape(15))
+                        Column(
+                            Modifier
+                                .fillMaxWidth()
                                 .border(1.5.dp, Color.LightGray, RoundedCornerShape(15))
-                            )
+                                .padding(8.dp)){
+                            Row (modifier = Modifier.fillMaxWidth()){
+                                Text(text = "Caracteres Especiais ?",
+                                    modifier = Modifier.padding(end = 10.dp),
+                                    fontFamily = fontSora, fontWeight = FontWeight.SemiBold,
+                                    color = Color.Black)
+                                Checkbox(checked = true,
+                                    onCheckedChange = validarEscolhaQtdCaracters(),
+                                    modifier = Modifier
+                                        .background(Color.White, RoundedCornerShape(15)),
+                                    colors = CheckboxDefaults.colors(checkedColor = Color.Blue, uncheckedColor = Color.LightGray))
+                            }
+                            Row(modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(top = 16.dp)){
+                                Text(text = "Com números ?",
+                                    modifier = Modifier.padding(end = 10.dp),
+                                    fontFamily = fontSora, fontWeight = FontWeight.SemiBold,
+                                    color = Color.Black)
+                                Checkbox(checked = true,
+                                    onCheckedChange = validarEscolhaQtdCaracters(),
+                                    modifier = Modifier
+                                        .background(Color.White, RoundedCornerShape(15)),
+                                    colors = CheckboxDefaults.colors(checkedColor = Color.Blue, uncheckedColor = Color.LightGray, checkmarkColor = Color.White))
+                            }
+                            Row(modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(top = 16.dp)){
+                                Text(text = "Qtd de caracteres", fontFamily = fontSora, fontWeight = FontWeight.SemiBold, color = Color.Black)
+                            }
                         }
-                         Row(modifier = Modifier.fillMaxWidth().padding(top = 16.dp)){
-                             Text(text = "Com números ?",
-                                 modifier = Modifier.padding(end = 10.dp),
-                                 fontFamily = fontSora, fontWeight = FontWeight.SemiBold,
-                                 color = Color.Black)
-                             Checkbox(checked = true, onCheckedChange = validarEscolhaQtdCaracters(), modifier = Modifier
-                                 .background(Color.White, RoundedCornerShape(15))
-                                 .border(1.5.dp, Color.LightGray, RoundedCornerShape(15))
-                             )
-                         }
+                        Column(
+                            Modifier
+                                .fillMaxWidth()
+                                .padding(top = 16.dp)
+                                .border(1.5.dp, Color.LightGray, RoundedCornerShape(20))){
+                            Box(modifier = Modifier
+                                .wrapContentHeight()
+                                .padding(8.dp)){
+                                Column(){
+                                    Text(text = "Gere sua senha com o botão abaixo",
+                                        fontFamily = fontSora, fontWeight = FontWeight.SemiBold)
 
+                                    ElevatedButton(onClick = { /*TODO*/ },
+                                        shape = ButtonDefaults.elevatedShape,
+                                        colors = ButtonDefaults.buttonColors(Color.Blue),
+                                        elevation = ButtonDefaults.buttonElevation(defaultElevation = 10.dp))
+                                    {
+                                        Text(text  = "Gerar Senha", fontFamily = fontSora, fontWeight = FontWeight.ExtraBold)
+                                    }
+                                }
+                            }
+                        }
                     }
+                }
             }
         }
-    }
 }
+
 
 fun validarEscolhaQtdCaracters(): ((Boolean) -> Unit)? {
     return null
+}
+
+fun validarQtdCaracteres(): (Float) -> Unit? {
+    return TODO("Provide the return value")
 }
 
 @Preview(showBackground = true)
